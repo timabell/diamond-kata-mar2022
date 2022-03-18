7b8d76afef7b4ea8bcee49a7edb76a3f7485f028
Tim Abell <tim@timwise.co.uk>
2022-03-18 23:14:30 +0000

    Blank test project
    ```
    dotnet new nunit --name diamond-tests
    ```

---
+++ diamond-tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace diamond_tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
