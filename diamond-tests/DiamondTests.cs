363c5b5772eb1ed257fbdce893decc4253e2ae56
Tim Abell <tim@timwise.co.uk>
2022-03-19 01:55:16 +0000

    Format test file
    Would normally squash this into a sensible patch

--- diamond-tests/DiamondTests.cs
+++ diamond-tests/DiamondTests.cs
@@ -5,20 +5,20 @@ namespace diamond_tests;
 
 public class DiamondTests
 {
-	[Test]
-	public void SingleChar()
-	{
-		var actual = new DiamondInTheAscii().Bling('A');
-		actual.Should().Be("A");
-	}
+    [Test]
+    public void SingleChar()
+    {
+        var actual = new DiamondInTheAscii().Bling('A');
+        actual.Should().Be("A");
+    }
 
-	[Test]
-	public void DiamondAB()
-	{
-		var actual = new DiamondInTheAscii().Bling('B');
-		actual.Should().Be(
-			" A\n" +
-			"B B\n"+
-			" A\n");
-	}
+    [Test]
+    public void DiamondAB()
+    {
+        var actual = new DiamondInTheAscii().Bling('B');
+        actual.Should().Be(
+            " A\n" +
+            "B B\n" +
+            " A\n");
+    }
 }
